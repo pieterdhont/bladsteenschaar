0c5f21d7c217297698415e6220dbb7bed66df878
Pjotterismus <dhontpieter22@gmail.com>
2023-04-16 15:35:55 +0200

    first commit

---
+++ spel.js
@@ -0,0 +1,39 @@
+"use strict"
+document.querySelectorAll("img").forEach(afbeelding => afbeelding.onclick = speelSpel);
+
+    function speelSpel() {
+      const gebruikersKeuze = this.dataset.keuze;
+      const computerKeuze = getComputerKeuze();
+      const winnaarTekst = bepaalWinnaar(gebruikersKeuze, computerKeuze);
+
+      const computerkeuzeAfbeelding = document.querySelector('#computerkeuze img');
+      toonComputerKeuze(computerkeuzeAfbeelding, computerKeuze);
+
+      document.getElementById('winnaar').innerText = winnaarTekst;
+    }
+
+    function toonComputerKeuze(img, computerKeuze) {
+      img.src = `${computerKeuze}.png`;
+      img.alt = computerKeuze;
+      img.title = computerKeuze;
+      
+    }
+
+    function getComputerKeuze() {
+      const keuzes = ['steen', 'blad', 'schaar']; 
+      return keuzes[Math.floor(Math.random() * 3)];
+    }
+
+    function bepaalWinnaar(gebruikersKeuze, computerKeuze) {
+      if (gebruikersKeuze === computerKeuze) {
+        return 'gelijkspel!';
+      }
+
+      const winnendeKeuzes = {
+        steen: 'schaar',
+        blad: 'steen',
+        schaar: 'blad'
+      };
+
+      return (winnendeKeuzes[gebruikersKeuze] === computerKeuze) ? 'Jij wint!' : 'Computer wint!';
+    }
